6d9635540da1bf74336b9e15a6ac141f94d40692
MoniRom YI <moniromyi@gmail>
2025-12-15 21:01:01 +0700

    update security conffig and add SupervisorController

--- demo/src/main/java/project/demo/Controller.java
+++
@@ -1,5 +0,0 @@
-package project.demo;
-
-public class Controller {
-    
-}
--- demo/src/main/java/project/demo/SecurityConfig.java
+++ demo/src/main/java/project/demo/SecurityConfig.java
@@ -4,13 +4,17 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
+
+@EnableMethodSecurity
 @Configuration
 public class SecurityConfig{
+
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
@@ -22,8 +26,11 @@ public class SecurityConfig{
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
         http
+            .csrf(csrf -> csrf.disable())
             .authorizeHttpRequests(auth -> 
-                auth.requestMatchers("/login").permitAll().anyRequest().authenticated()
+                auth.requestMatchers("/login").permitAll()
+                .requestMatchers("/supervisor/").hasRole("/SUPERVISOR")
+                .anyRequest().authenticated()
             )
             .formLogin(form -> 
                 form.loginPage("/login")
