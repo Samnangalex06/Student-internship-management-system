07f7eacc0b5b3014f51d42366217f09eceff0520
Yi-Monirom <moniromyi@gmail.com>
2025-12-22 19:01:12 +0700

    delete and moodify controler

--- demo/src/main/resources/db/migrations/V5__create_table_supervisors.sql
+++
@@ -1,6 +0,0 @@
-create table supervisors (
-    id serial primary key,
-    name varchar(100) not null,
-    department varchar(100) not null,
-    created_at timestamp default current_timestamp
-);
